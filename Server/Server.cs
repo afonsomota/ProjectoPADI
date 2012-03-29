0d9152b8dd32c5f56f4cc19b99efe7922f1b6c59
Afonso Oliveira <afonso.oliveira@ist.utl.pt>
2012-03-29 18:37:25 +0100

    Semitables implementation started

--- Server/Server.cs
+++ Server/Server.cs
@@ -7,13 +7,19 @@ using System.Runtime.Remoting.Channels.Tcp;
 using System.Runtime.Remoting;
 using CommonInterfaces;
 using System.Threading;
+using System.Collections;
 
 namespace Server
 {
+    public struct TableValue
+    {
+        public string Value;
+        public int Timestamp;
+    } ;
 
     class Program
     {
-
+        
 
         static void Main(string[] args)
         {
@@ -29,7 +35,7 @@ namespace Server
                typeof(IPuppetMaster),
                "tcp://localhost:8090/PseudoNodeReg");
             Node node = new Node(host, port, NodeType.Server);
-            Server srv = new Server(node, channel);
+            Server srv = new Server(node, channel,5);
             ServerPuppet.ctx = srv;
             ServerRemoting.ctx = srv;
             ligacao.RegisterPseudoNode(node);
@@ -43,11 +49,68 @@ namespace Server
         public Node Info;
         public TcpChannel Channel;
         public List<Node> NetworkTopology;
+        private Dictionary<string, List<TableValue>>[] Semitables;
+        public int K;
 
-        public Server(Node info, TcpChannel channel)
+        public Server(Node info, TcpChannel channel, int k)
         {
             Info = info;
             Channel = channel;
+            Semitables = new Dictionary<string, List<TableValue>>[2];
+            Semitables[0] = new Dictionary<string, List<TableValue>>();
+            Semitables[1] = new Dictionary<string, List<TableValue>>();
+            K = k;
+        }
+
+        public string Get(string key, int timestamp) {
+            foreach (Dictionary<string, List<TableValue>> st in Semitables)
+                if(st.ContainsKey(key))
+                    foreach (TableValue tv in st[key]) 
+                        if (tv.Timestamp == timestamp)
+                            return tv.Value;
+            return null;
+        }
+
+        public Dictionary<string, List<TableValue>> GetAll() { 
+
+            Dictionary<string, List<TableValue>> all = new Dictionary<string, List<TableValue>>();
+            foreach (Dictionary<string, List<TableValue>> st in Semitables) { 
+                foreach(string key in st.Keys){
+                    all.Add(key, st[key]);
+                }
+            }
+            return all;
+        }
+
+        public string Put(string key, string value)
+        {
+            foreach (Dictionary<string, List<TableValue>> st in Semitables)
+                if (st.ContainsKey(key))
+                {
+                    int max_timestamp = 0;
+                    foreach (TableValue tv in st[key]){
+                        max_timestamp = tv.Timestamp;
+                    }
+                    if (st[key].Count == K)
+                        foreach (TableValue tv in st[key])
+                            if (tv.Timestamp == max_timestamp){
+                                st[key].Remove(tv);
+                                break;
+                            }
+                    TableValue newtv = new TableValue();
+                    newtv.Timestamp = max_timestamp + 1;
+                    newtv.Value = value;
+                    st[key].Add(newtv);
+                }
+                else { 
+                    TableValue tv = new TableValue();
+                    tv.Timestamp = 0;
+                    tv.Value = value;
+                    List<TableValue> values = new List<TableValue>();
+                    values.Add(tv);
+                    st.Add(key, values);
+                }
+            return null;
         }
     }
 

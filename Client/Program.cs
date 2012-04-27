b99eef2db9d176f7f8892b8295edaa5a847d300b
Afonso Oliveira <afonso.oliveira@ist.utl.pt>
2012-04-27 19:33:58 +0100

    Client compiling

--- Client/Program.cs
+++ Client/Program.cs
@@ -142,18 +142,17 @@ namespace Client
 
 
         }
-
-    }
-
     class ClientRemoting : MarshalByRefObject, IClient
     {
 
-        public static Client.Client ctx;
+        public static Client ctx;
 
-        public void GetNetworkUpdate(List<Node> network) {
+        public void GetNetworkUpdate(List<Node> network)
+        {
             ctx.NetworkTopology = network;
             Console.WriteLine("\nNetwork Topology Update!");
-            foreach (Node n in ctx.NetworkTopology) {
+            foreach (Node n in ctx.NetworkTopology)
+            {
                 Console.WriteLine(n);
             }
         }
@@ -161,12 +160,12 @@ namespace Client
 
     class ClientPuppet : MarshalByRefObject, IClientPuppet
     {
-        public static Client.Client ctx;
+        public static Client ctx;
 
         public void StartClient()
-        { 
+        {
             ctx.CD.RegisterClient(ctx.Info);
-            Console.WriteLine("Client Online");   
+            Console.WriteLine("Client Online");
         }
 
 
@@ -185,57 +184,66 @@ namespace Client
         }
 
         //Adiciona o valor ao registo
-        public void Store(int register, string value) {
+        public void Store(int register, string value)
+        {
             ctx.StoreInternal(register, value);
         }
 
         //Executa um Put com o conteudo do registo "register" na key "key"
-        public void Put(int register, string key) {
+        public void Put(int register, string key)
+        {
             ctx.PutInternal(register, key);
         }
 
         //Executa um get na key "key" e guarda o conteudo no register
-        public void Get(int register, string key) {
+        public void Get(int register, string key)
+        {
             ctx.GetInternal(register, key);
         }
 
         //mete o valor "value" na key "key"
-        public void PutVAl(string key, int value) {
+        public void PutVAl(string key, int value)
+        {
             ctx.PutVAlInternal(key, value);
         }
 
         //change the value of the key in the register number to lower case
-        public void ToLower(int register) {
-            ctx.ToLowerInternal(register-1);
+        public void ToLower(int register)
+        {
+            ctx.ToLowerInternal(register - 1);
         }
 
-        public void ToUpper(int register) {
-            ctx.ToUpperInternal(register-1);
+        public void ToUpper(int register)
+        {
+            ctx.ToUpperInternal(register - 1);
         }
 
         //concata a string do registo 2 no registo 1
-        public void Concat(int register1, int register2) {
+        public void Concat(int register1, int register2)
+        {
             ctx.ConcatInternal(register1, register2);
         }
 
         //faz commit da transacção
-        public void CommitTx() {
+        public void CommitTx()
+        {
             ctx.CommitTxInternal();
         }
 
-        public string[] Dump() {
+        public string[] Dump()
+        {
             return ctx.Registers;
         }
 
         public void ExeScript(List<string> instructions)
         {
-            Dictionary<int,List<string>> clientOperations = new Dictionary<int,List<string>>();
+            Dictionary<int, List<string>> clientOperations = new Dictionary<int, List<string>>();
             int numberOfTransactions = -1;
             bool inTransaction = false;
 
             for (; instructions.Count != 0; instructions.Remove(instructions[0]))
             {
-                
+
 
                 if (instructions[0].StartsWith("BEGINTX"))
                 {
@@ -259,9 +267,9 @@ namespace Client
                     char[] delim = { ' ', '\t' };
                     string[] arg = instructions[0].Split(delim);
 
-                    if (instructions[0].StartsWith("GET")) 
-                    { 
-                    
+                    if (instructions[0].StartsWith("GET"))
+                    {
+
                     }
                     else if (instructions[0].StartsWith("PUT"))
                     {
@@ -281,24 +289,47 @@ namespace Client
                         this.Dump();
                 }
             }
-    }
+        }
 
-    public void ExecuteTransactions(Dictionary<int,List<string>> clientOperations)
-    {
-        List<Operation> operationsLocations = new List<Operation>();
-        char[] delim = { ' ', '\t' };
-
-        for (int aux2 = 1; clientOperations.Count != aux2; aux2++ )
+        public void ExecuteTransactions(Dictionary<int, List<string>> clientOperations)
         {
-            operationsLocations = GetOperationsLocation(clientOperations[aux2]);
+            List<Operation> operationsLocations = new List<Operation>();
+            char[] delim = { ' ', '\t' };
 
-            //Nao ha gets nem puts... é correr tudo....
-            if (operationsLocations.Count == 0)
+            for (int aux2 = 1; clientOperations.Count != aux2; aux2++)
             {
+               // operationsLocations = GetOperationsLocation(clientOperations[aux2]);
+
+                //Nao ha gets nem puts... é correr tudo....
+                if (operationsLocations.Count == 0)
+                {
+                    for (int aux = 1; clientOperations[aux].Count != aux; aux++)
+                    {
+                        string[] arg = clientOperations[aux][aux2].Split(delim);
+
+                        if (clientOperations[aux][aux2].StartsWith("STORE"))
+                            this.Store(Int32.Parse(arg[1]), arg[2]);
+                        else if (clientOperations[aux][aux2].StartsWith("PUTVAL"))
+                            this.PutVAl(arg[1], Int32.Parse(arg[2]));
+                        else if (clientOperations[aux][aux2].StartsWith("TOLOWER"))
+                            this.ToLower(Int32.Parse(arg[1]));
+                        else if (clientOperations[aux][aux2].StartsWith("TOUPPER"))
+                            this.ToUpper(Int32.Parse(arg[1]));
+                        else if (clientOperations[aux][aux2].StartsWith("CONCAT"))
+                            this.Concat(Int32.Parse(arg[1]), Int32.Parse(arg[2]));
+                        else if (clientOperations[aux][aux2].StartsWith("DUMP"))
+                            this.Dump();
+                    }
+                }
+
+                Transaction transaction = new Transaction();
+
                 for (int aux = 1; clientOperations[aux].Count != aux; aux++)
                 {
+
                     string[] arg = clientOperations[aux][aux2].Split(delim);
 
+                    //standart stuff
                     if (clientOperations[aux][aux2].StartsWith("STORE"))
                         this.Store(Int32.Parse(arg[1]), arg[2]);
                     else if (clientOperations[aux][aux2].StartsWith("PUTVAL"))
@@ -311,75 +342,245 @@ namespace Client
                         this.Concat(Int32.Parse(arg[1]), Int32.Parse(arg[2]));
                     else if (clientOperations[aux][aux2].StartsWith("DUMP"))
                         this.Dump();
+
+
+                    //get e sets...
+                    //else if (clientOperations[aux][aux2].StartsWith("GET"))
+                    //{
+                    //    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + transaction.NodesLocation[arg[2]] + "/Server");
+                    //    ctx.Registers[Int32.Parse(arg[1])] = server.Get(transaction.Txid, arg[2]);
+
+                    //}
+                    //else if (clientOperations[aux][aux2].StartsWith("SET"))
+                    //{
+                    //    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + transaction.NodesLocation[arg[2]] + "/Server");
+                    //    server.Put(transaction.Txid, arg[1], ctx.Registers[Int32.Parse(arg[1])]);
+                    //}
                 }
-            }
 
-            TransactionContext transaction = ctx.CD.GetServers(operationsLocations);
-
-            for (int aux = 1; clientOperations[aux].Count != aux; aux++) 
-            {
-                
-                string[] arg = clientOperations[aux][aux2].Split(delim);
-
-                //standart stuff
-                if (clientOperations[aux][aux2].StartsWith("STORE"))
-                    this.Store(Int32.Parse(arg[1]), arg[2]);
-                else if (clientOperations[aux][aux2].StartsWith("PUTVAL"))
-                    this.PutVAl(arg[1], Int32.Parse(arg[2]));
-                else if (clientOperations[aux][aux2].StartsWith("TOLOWER"))
-                    this.ToLower(Int32.Parse(arg[1]));
-                else if (clientOperations[aux][aux2].StartsWith("TOUPPER"))
-                    this.ToUpper(Int32.Parse(arg[1]));
-                else if (clientOperations[aux][aux2].StartsWith("CONCAT"))
-                    this.Concat(Int32.Parse(arg[1]), Int32.Parse(arg[2]));
-                else if (clientOperations[aux][aux2].StartsWith("DUMP"))
-                    this.Dump();
-
-            
-                //get e sets...
-                else if (clientOperations[aux][aux2].StartsWith("GET"))
-                {
-                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + transaction.NodesLocation[arg[2]] + "/Server");
-                    ctx.Registers[Int32.Parse(arg[1])] = server.Get(transaction.Txid,arg[2]);
-                    
-                }
-                else if (clientOperations[aux][aux2].StartsWith("SET"))
-                {
-                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + transaction.NodesLocation[arg[2]] + "/Server");
-                    server.Put(transaction.Txid, arg[1], ctx.Registers[Int32.Parse(arg[1])]);
-                }
             }
 
         }
-        
+
     }
 
-    public List<Operation> GetOperationsLocation(List<string> transactions)
+    class Transaction
     {
-        List<Operation> operationsLocations = new List<Operation>();
 
-        for (int aux = 1; aux != transactions.Count; aux++)
+        List<string> AccessedKeys;
+        Dictionary<string, List<Node>> NodesLocation;
+        List<Node> Nodes;
+        TransactionContext Tctx;
+        ICentralDirectory Central;
+
+        public Transaction()
         {
-
-            if (transactions[aux].StartsWith("GET"))
+            AccessedKeys = new List<string>();
+            NodesLocation = new Dictionary<string, List<Node>>();
+            Nodes = new List<Node>();
+            Central = (ICentralDirectory)Activator.GetObject(
+              typeof(ICentralDirectory),
+              "tcp://localhost:9090/CentralDirectory");
+            while (true)
             {
-                char[] delim = { ' ', '\t' };
-                string[] arg = transactions[0].Split(delim);
+                Tctx = Central.BeginTx();
+                if (Tctx.Txid != -1)
+                    break;
 
-                operationsLocations.Add(new Operation(arg[2]));
+                Thread.Sleep(500);
 
             }
-            else if (transactions[aux].StartsWith("PUT"))
-            {
-                char[] delim = { ' ', '\t' };
-                string[] arg = transactions[0].Split(delim);
+            Console.WriteLine(Tctx);
 
-                operationsLocations.Add(new Operation(arg[2], arg[2]));
+        }
+
+        List<Node> GetAndLockKey(string key)
+        {
+            List<Node> nodes = Central.GetServers(key);
+            foreach (Node n in nodes)
+                if (!Nodes.Contains(n))
+                    Nodes.Add(n);
+            NodesLocation.Add(key, nodes);
+            bool allCanLock = true;
+            bool allLocked = true;
+            foreach (Node n in nodes)
+            {
+                IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                if (!server.CanLock(Tctx.Txid, key)) allCanLock = false;
+            }
+            if (!allCanLock)
+            {
+                Abort();
+                return null;
+            }
+            else
+            {
+                foreach (Node n in nodes)
+                {
+                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                    if (!server.Lock(Tctx.Txid, key))
+                    {
+                        allLocked = false;
+                        break;
+                    }
+                }
+                if (!allLocked)
+                {
+                    Abort();
+                    return null;
+                }
+            }
+            return nodes;
+        }
+
+        public string GetValue(string key)
+        {
+            List<Node> nodes = null;
+            if (AccessedKeys.Contains(key))
+            {
+                nodes = NodesLocation[key];
+            }
+            else
+            {
+                nodes = GetAndLockKey(key);
+                AccessedKeys.Add(key);
+            }
+            if (nodes != null)
+            {
+                IServer serv = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + nodes[0].IP + ":" + nodes[0].Port.ToString() + "/Server");
+                string value;
+                try
+                {
+                    value = serv.Get(Tctx.Txid, key);
+                }
+                catch
+                {
+                    IServer servBackup = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + nodes[1].IP + ":" + nodes[1].Port.ToString() + "/Server");
+                    try
+                    {
+                        value = servBackup.Get(Tctx.Txid, key);
+                        Central.ServerDown(nodes[0]);
+                    }
+                    catch
+                    {
+                        value = null;
+                        throw;
+                    }
+                    throw;
+                }
+                if (value == null)
+                {
+                    Abort();
+                    return null;
+                }
+                else return value;
+            }
+            else return null;
+        }
+
+        public bool PutValue(string key, string value)
+        {
+            List<Node> nodes = null;
+            if (AccessedKeys.Contains(key))
+            {
+                nodes = NodesLocation[key];
+            }
+            else
+            {
+                nodes = GetAndLockKey(key);
+                AccessedKeys.Add(key);
+            }
+            if (nodes != null)
+            {
+                IServer serv = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + nodes[0].IP + ":" + nodes[0].Port.ToString() + "/Server");
+                bool success;
+                try
+                {
+                    success = serv.Put(Tctx.Txid, key, value);
+                }
+                catch
+                {
+                    IServer servBackup = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + nodes[1].IP + ":" + nodes[1].Port.ToString() + "/Server");
+                    try
+                    {
+                        success = servBackup.Put(Tctx.Txid, key, value);
+                        Central.ServerDown(nodes[0]);
+                    }
+                    catch
+                    {
+                        success = false;
+                        throw;
+                    }
+                    throw;
+                }
+                if (!success)
+                {
+                    Abort();
+                    return false;
+                }
+                else return success;
+            }
+            else return false;
+        }
+
+        public bool Commit()
+        {
+            bool allCanCommit = true;
+            Node nodeToRemove = null;
+            foreach (Node n in Nodes)
+            {
+                try
+                {
+                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                    if (!server.CanCommit(Tctx.Txid))
+                    {
+                        allCanCommit = false;
+                        break;
+                    }
+                }
+                catch
+                {
+                    nodeToRemove = n;
+                    Central.ServerDown(n);
+                }
+            }
+            if (nodeToRemove != null) Nodes.Remove(nodeToRemove);
+            Tctx.State = TransactionContext.states.tentatively;
+            Console.WriteLine(Tctx);
+            if (!allCanCommit)
+            {
+                Abort();
+                return false;
+            }
+            else
+            {
+                foreach (Node n in Nodes)
+                {
+                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                    server.Commit(Tctx.Txid);
+                }
+                Tctx.State = TransactionContext.states.commited;
+                Central.UpdateTransactionState(Tctx);
+                Console.WriteLine(Tctx);
+                return true;
             }
         }
-        return operationsLocations;
+
+        public void Abort()
+        {
+            foreach (Node n in Nodes)
+            {
+                IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                server.Abort(Tctx.Txid);
+            }
+            Tctx.State = TransactionContext.states.aborted;
+            Central.UpdateTransactionState(Tctx);
+            Console.WriteLine(Tctx);
+
+        }
+
     }
 
-    
+    }
 
-}
+  

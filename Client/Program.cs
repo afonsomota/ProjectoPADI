47529325910e4ad11263d03399191f144f4f951a
golfadas <bersimoes@gmail.com>
2012-04-30 17:51:31 +0100

    transaction updated (server kill awareness)

--- Client/Program.cs
+++ Client/Program.cs
@@ -395,28 +395,46 @@ namespace Client
             NodesLocation.Add(key, nodes);
             bool allCanLock = true;
             bool allLocked = true;
+            int serversDown = 0;
             foreach (Node n in nodes)
             {
-                IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
-                if (!server.CanLock(Tctx.Txid, key)) allCanLock = false;
+                try
+                {
+                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                    if (!server.CanLock(Tctx.Txid, key)) allCanLock = false;
+                }
+                catch
+                {
+                    serversDown++;
+                    Central.ServerDown(n);
+                }
             }
-            if (!allCanLock)
+            if (!allCanLock || serversDown == 2)
             {
                 Abort();
                 return null;
             }
             else
             {
+                serversDown = 0;
                 foreach (Node n in nodes)
                 {
-                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
-                    if (!server.Lock(Tctx.Txid, key))
+                    try
                     {
-                        allLocked = false;
-                        break;
+                        IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                        if (!server.Lock(Tctx.Txid, key))
+                        {
+                            allLocked = false;
+                            break;
+                        }
+                    }
+                    catch
+                    {
+                        serversDown++;
+                        Central.ServerDown(n);
                     }
                 }
-                if (!allLocked)
+                if (!allLocked || serversDown == 2)
                 {
                     Abort();
                     return null;
@@ -456,9 +474,7 @@ namespace Client
                     catch
                     {
                         value = null;
-                        throw;
                     }
-                    throw;
                 }
                 if (value == null)
                 {
@@ -501,9 +517,7 @@ namespace Client
                     catch
                     {
                         success = false;
-                        throw;
                     }
-                    throw;
                 }
                 if (!success)
                 {
@@ -519,6 +533,7 @@ namespace Client
         {
             bool allCanCommit = true;
             Node nodeToRemove = null;
+            int serversDown = 0;
             foreach (Node n in Nodes)
             {
                 try
@@ -532,6 +547,7 @@ namespace Client
                 }
                 catch
                 {
+                    serversDown++;
                     nodeToRemove = n;
                     Central.ServerDown(n);
                 }
@@ -539,7 +555,7 @@ namespace Client
             if (nodeToRemove != null) Nodes.Remove(nodeToRemove);
             Tctx.State = TransactionContext.states.tentatively;
             Console.WriteLine(Tctx);
-            if (!allCanCommit)
+            if (!allCanCommit || serversDown == 2)
             {
                 Abort();
                 return false;
@@ -562,8 +578,15 @@ namespace Client
         {
             foreach (Node n in Nodes)
             {
-                IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
-                server.Abort(Tctx.Txid);
+                try
+                {
+                    IServer server = (IServer)Activator.GetObject(typeof(IServer), "tcp://" + n.IP + ":" + n.Port.ToString() + "/Server");
+                    server.Abort(Tctx.Txid);
+                }
+                catch
+                {
+                    Central.ServerDown(n);
+                }
             }
             Tctx.State = TransactionContext.states.aborted;
             Central.UpdateTransactionState(Tctx);
